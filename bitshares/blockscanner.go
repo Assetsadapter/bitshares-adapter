845d9f99123e95b4de14a29aec5684da75248a61
nobody <nobody>
2020-09-07 20:43:15 +0800

    mod:比对扫描代码

--- bitshares/blockscanner.go
+++ bitshares/blockscanner.go
@@ -31,7 +31,7 @@ import (
 )
 
 const (
-	blockchainBucket = "blockchain" // blockchain dataset
+	//blockchainBucket = "blockchain" // blockchain dataset
 	//periodOfTask      = 5 * time.Second // task interval
 	maxExtractingSize = 10 // thread count
 )
@@ -248,7 +248,7 @@ func (bs *BtsBlockScanner) BatchExtractTransactions(blockHeight uint64, blockHas
 				}
 			} else {
 				//记录未扫区块
-				unscanRecord := NewUnscanRecord(height, "", "")
+				unscanRecord := openwallet.NewUnscanRecord(height, "", "", bs.wm.Symbol())
 				bs.SaveUnscanRecord(unscanRecord)
 				failed++ //标记保存失败数
 			}
@@ -617,8 +617,8 @@ func (bs *BtsBlockScanner) newExtractDataNotify(height uint64, extractData map[s
 				if err != nil {
 					log.Error("BlockExtractDataNotify unexpected error:", err)
 					//记录未扫区块
-					unscanRecord := NewUnscanRecord(height, "", "ExtractData Notify failed.")
-					err = bs.SaveUnscanRecord(unscanRecord)
+					unscanRecord := openwallet.NewUnscanRecord(height, "", "", bs.wm.Symbol())
+					bs.SaveUnscanRecord(unscanRecord)
 					if err != nil {
 						log.Std.Error("block height: %d, save unscan record failed. unexpected error: %v", height, err.Error())
 					}
@@ -653,7 +653,7 @@ func (bs *BtsBlockScanner) scanBlock(height uint64) (*Block, error) {
 		bs.wm.Log.Std.Info("block scanner can not get new block data; unexpected error: %v", err)
 
 		//记录未扫区块
-		unscanRecord := NewUnscanRecord(height, "", err.Error())
+		unscanRecord := openwallet.NewUnscanRecord(height, "", err.Error(), bs.wm.Symbol())
 		bs.SaveUnscanRecord(unscanRecord)
 		bs.wm.Log.Std.Info("block height: %d extract failed.", height)
 		return nil, err
@@ -766,7 +766,7 @@ func (bs *BtsBlockScanner) RescanFailedRecord() {
 		blockMap = make(map[uint64][]string)
 	)
 
-	list, err := bs.wm.GetUnscanRecords()
+	list, err := bs.GetUnscanRecords()
 	if err != nil {
 		bs.wm.Log.Std.Info("block scanner can not get rescan data; unexpected error: %v", err)
 	}
@@ -811,5 +811,10 @@ func (bs *BtsBlockScanner) RescanFailedRecord() {
 	}
 
 	//删除未没有找到交易记录的重扫记录
-	bs.wm.DeleteUnscanRecordNotFindTX()
+}
+
+//SupportBlockchainDAI 支持外部设置区块链数据访问接口
+//@optional
+func (bs *BtsBlockScanner) SupportBlockchainDAI() bool {
+	return true
 }
